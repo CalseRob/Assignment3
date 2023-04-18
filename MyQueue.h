87a56ca78ffb947e08b632a900106ce935ddc4c5
CalseRob <100106687+CalseRob@users.noreply.github.com>
2023-04-17 23:46:58 -0500

    Create MyQueue.h

---
+++ MyQueue.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+
+class MyQueue{
+    private:
+      std::queue<int> q;
+
+    public:
+      //Function to insert an element at the rear of the queue 
+      void enqueue(int x){
+        
+      }
+
+      //Function to remove the front element of the queue and return its value
+      int dequeue(){
+        
+      }      
+
+      //Function to check if the queue is empty
+      bool isEmpty(){
+        
+      }
+
+      //Function to return the front element present in the queue without removing it
+      int peek(){
+        
+      }
+
+      //Function to return the total number of elements present in the queue
+      size_t size(){
+        
+      }
+
+    
+};
